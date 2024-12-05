579777b0e4096355e6e652b9e1975eeb9cb3f46d
macooo3 <marcoysu765@gmail.com>
2024-12-05 12:56:55 -0500

    completed set func

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
 class HashMap {
-constructor(loadFactor, capacity){
-    this.loadFactor = loadFactor
-    this.capacity = capacity
-    this.int = this.loadFactor * this.capacity
-    this.Map 
-}
+  constructor(loadFactor, capacity) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.int = this.loadFactor * this.capacity;
+    this.Map = [];
+  }
 
-
-hash(key) {
+  hash(key) {
     let hashCode = 0;
-    
+
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % int;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) %16 ;
     }
- 
+
     return hashCode;
-  } 
- 
-set(key, value) {
- const hashKey = this.hash(key)
-    
-}
+  }
 
-}
+  set(key, value) {
+    const hashKey = this.hash(key);
+    if (!this.Map[hashKey]) {
+      this.Map[hashKey] = {
+        key: key,
+        value: value,
+        node: null,
+      };
+    } else {
+      let current = this.Map[hashKey];
+      while (current.node != null && current.key != key) {
+        current = current.node;
+      }
+      if(current.key === key) {
+        current.value = value
+      }
+    else { current.node = {
+        key: key,
+        value: value,
+        node: null,
+      };}
+    }
 
+    return this.Map
+  }
+
+  get(key){
+    const hashKey = this.hash(key);
+    if (!this.Map[hashKey]){
+      this.Map[hashKey]
+    }
+  }
+  length(){
+    return this.Map.length
+  }
+}
 
 const test = new HashMap(16, 0.75);
-// test.set('apple', 'red')
-// test.set('banana', 'yellow')
-// test.set('carrot', 'orange')
-// test.set('dog', 'brown')
-// test.set('elephant', 'gray')
-// test.set('frog', 'green')
-// test.set('grape', 'purple')
-// test.set('hat', 'black')
-// test.set('ice cream', 'white')
-// test.set('jacket', 'blue')
-// test.set('kite', 'pink')
-// test.set('lion', 'golden')
+test.set("apple", "red");
+test.set("apple", "blue");
+test.set('banana', 'yellow') //9
+test.set('carrot', 'orange') //3
+test.set('dog', 'brown') //8
+test.set('elephant', 'gray') //9
+test.set('frog', 'green') //4
+test.set('grape', 'purple') //11
+test.set('hat', 'black') //11
+test.set('ice cream', 'white') //5
+test.set('jacket', 'blue') //2
+test.set('kite', 'pink') //3
+test.set('lion', 'golden') //8
+console.log(test.set('house', 'white'));
+console.log(test.length());
